4da0c9c2a6d16db24ae609d462647ca192cb43b6
unknown <edwardarahi2002@gmail.com>
2024-08-01 04:36:43 +0100

    commit

---
+++ schoolmanagement-frontend/src/app/services/course.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseService {
+  private url = "localhost:8082/courses";
+  constructor(private http : HttpClient) { }
+
+  addCourse(course : any) : Observable<any>{
+    const headers = new HttpHeaders().set('Authorization',`Bearer ${localStorage.getItem('token')}`);
+    return this.http.post<any>(this.url,course,{ headers });
+  }
+
+  getCourses() : Observable<any>{
+    const headers = new HttpHeaders().set('Authorization',`Bearer ${localStorage.getItem('token')}`);
+    return this.http.get(this.url,{ headers });
+  }
+
+  updateCourse(idCourse : number ,course : any) : Observable<any>{
+    const headers = new HttpHeaders().set('Authorization',`Bearer ${localStorage.getItem('token')}`);
+    const updateUrl = `${this.url}/${idCourse}`;
+    return this.http.put<any>(updateUrl,course,{ headers });
+  }
+
+  deleteCourse(idCourse : number) : Observable<any>{
+    const headers = new HttpHeaders().set('Authorization',`Bearer ${localStorage.getItem('token')}`);
+    const deleteUrl = `${this.url}/${idCourse}`;
+    return this.http.delete<any>(deleteUrl,{ headers });
+  }
+}
